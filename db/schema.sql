9574806897809ade9e5df81e98529db11dc75b87
Joey Brown <gjoey.brown@gmail.com>
2020-11-05 12:24:03 -0800

    added seeds.sql file within db folder

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
-CREATE TABLE (
+CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR NOT NULL,
     devoured BOOLEAN DEFAULT true,
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Sonoran Dog Burger');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Deluxe with Cashew Cheese');
+
