909a0748a7a0cca64a0a4f17bd86ea76a138b264
Joey Brown <gjoey.brown@gmail.com>
2020-11-11 14:09:25 -0800

    updating connection

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: "",
+        password: "Claptonrules77!!",
         database: "burger_db"
     });
 };
