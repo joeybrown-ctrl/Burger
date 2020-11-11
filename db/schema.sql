340082cc60d75b02fb9dc94d27f4d551cf357e9a
Joey Brown <gjoey.brown@gmail.com>
2020-11-11 14:16:41 -0800

    fixed schema file

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,5 @@ CREATE TABLE burgers (
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100),
     devoured BOOLEAN DEFAULT false,
-    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
